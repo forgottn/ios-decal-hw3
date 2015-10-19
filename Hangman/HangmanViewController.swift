c6fe6632b89cbc9d50e396252f85bf7656e1edeb
forgottn <pduong@berkeley.edu>
2015-10-18 18:30:13 -0700

    done

--- Hangman/HangmanViewController.swift
+++ Hangman/HangmanViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class HangmanViewController: UIViewController {
 
+    @IBOutlet var textField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -18,6 +19,12 @@ class HangmanViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        textField.becomeFirstResponder()
+        
     }
 }
 
